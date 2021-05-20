b3b3e14854741536ccb064ebdea3034cd1d9afaf
Dmytruk Serhii <dmytruk.serhii@gmail.com>
2021-05-20 04:09:26 +0300

    2.4_3 done

--- app/code/Training/Test/Controller/Index/Index.php
+++ app/code/Training/Test/Controller/Index/Index.php
@@ -4,8 +4,19 @@ namespace Training\Test\Controller\Index;
 
 class Index extends \Magento\Framework\App\Action\Action
 {
+    private $resultRawFactory;
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\Controller\Result\RawFactory $resultRawFactory
+    ) {
+        $this->resultRawFactory = $resultRawFactory;
+        parent::__construct($context);
+    }
     public function execute()
     {
-        $this->getResponse()->appendBody('simple text');
+        $resultRaw = $this->resultRawFactory->create();
+        $resultRaw->setContents('simple text123');
+        return $resultRaw;
     }
 }
